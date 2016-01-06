4f4f5539cfd7a391add53d1a545ea1713c680b76
Alien Goo <glenn.android@gmail.com>
2016-01-06 22:10:55 +0000

    wip

---
+++ src/TaComponents/Controllers/ComponentProductStatusController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNet.Mvc;
+using TaComponents.Models;
+
+namespace TaComponents.Controllers
+{
+    [Route("api/product-statuses")]
+    public class ComponentProductStatusController : Controller
+    {
+        [HttpGet]
+        public string[] Get()
+        {
+            return Enum.GetNames(typeof(ComponentProductStatus));
+        }
+    }
+}
---
+++ src/TaComponents/Repositories/ActiveDirectory/FakeUserRepository.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaComponents.Models;
+
+namespace TaComponents.Repositories.ActiveDirectory
+{
+    public class FakeUserRepository : IUserRepository
+    {
+        public Task<IEnumerable<User>> GetAppMembers()
+        {
+
+            var dic = new Dictionary<string, string>
+            {
+                { "Homer", "Simpson"},
+                { "Richard", "Harris"},
+                { "Gordon", "Smith"},
+                { "Fox", "Mulder"},
+                { "Dana", "Scully"},
+                { "Leo", "McGarry"},
+                { "Jeb", "Bartlett"},
+                { "Pepper", "Potts"},
+                { "Tony", "Stark"},
+                { "Jimmy", "Fallon"},
+                { "Gillian", "Anderson"},
+                { "David", "Duchovony"},
+                { "Slim", "Pickens"},
+                { "Robert", "De Niro"},
+                { "Al", "Pacino"},
+                { "Josh", "Liman"},
+                { "Debbie", "Dingle"},
+            };
+
+            var fakeUsers = dic.Select((kvp) =>
+            {
+                var samAccountName = $"{kvp.Key[0]}{kvp.Value}".ToLower().Replace(" ", "");
+                return new User
+                {
+                    FirstName = kvp.Key,
+                    LastName = kvp.Value,
+                    SamAccountName = samAccountName,
+                    Email = $"{samAccountName}@test.com"
+
+                };
+            });
+            
+            return Task.FromResult(fakeUsers);
+        }
+    }
+}
