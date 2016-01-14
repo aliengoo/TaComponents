51961ce2eb1cb4a979239a3a3397efe03cca1e09
aliengoo <glenn.aliengoo@gmail.com>
2016-01-14 15:52:16 +0000

    wip

---
+++ src/TaComponents/src/thing/components/ThingDescription/ThingDescriptionView.js
@@ -0,0 +1,17 @@
+"use strict";
+
+import React, {Component, PropTypes} from "react";
+
+export default class ThingDescriptionView {
+  render() {
+    return (
+      <div className="ThingDescriptionView">
+
+      </div>
+    );
+  }
+}
+
+ThingDescriptionView.defaultProps = {};
+
+ThingDescriptionView.propTypes = {};
